1d1a607faa173048786937b0cebbf5444c457b75
Krish Chawla <krish.chawla@scsstudent.org>
2025-11-11 13:00:02 -0800

    feat: Complete Video 08

--- app/onboarding.tsx
+++ app/onboarding.tsx
@@ -1,8 +1,10 @@
 import { PlantlyButton } from "@/components/PlantlyButton";
 import { useUserStore } from "@/store/userStore";
 import { theme } from "@/theme";
+import { LinearGradient } from "expo-linear-gradient";
 import { useRouter } from "expo-router";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet } from "react-native";
 
 export default function OnboardingScreen() {
   const router = useRouter();
@@ -12,9 +14,15 @@ export default function OnboardingScreen() {
     router.replace("/");
   };
   return (
-    <View style={styles.container}>
+    <LinearGradient
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1, y: 1 }}
+      colors={[theme.colorGreen, theme.colorAppleGreen, theme.colorLimeGreen]}
+      style={styles.container}
+    >
+      <StatusBar style="light" />
       <PlantlyButton title="Let Me In!!" onPress={handlePress} />
-    </View>
+    </LinearGradient>
   );
 }
 

94927b64080dd08d7051b6b4803812b01ce79b18
Krish Chawla <krish.chawla@scsstudent.org>
2025-11-11 13:34:19 -0800

    feat: Complete Video 10

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,6 +3,9 @@ import Entypo from "@expo/vector-icons/Entypo";
 import Feather from "@expo/vector-icons/Feather";
 import { theme } from "@/theme";
 import { useUserStore } from "@/store/userStore";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import { Pressable } from "react-native";
+import { Link } from "expo-router";
 
 export default function Layout() {
   const hasFinishedOnboarding = useUserStore(
@@ -21,6 +24,17 @@ export default function Layout() {
           tabBarShowLabel: false,
           tabBarIcon: ({ size, color }) => (
             <Entypo name="leaf" size={size} color={color} />
+          ),
+          headerRight: () => (
+            <Link href="/new" asChild>
+              <Pressable style={{ marginRight: 18 }} hitSlop={20}>
+                <AntDesign
+                  name="plus-circle"
+                  size={24}
+                  color={theme.colorGreen}
+                />
+              </Pressable>
+            </Link>
           ),
         }}
       />
